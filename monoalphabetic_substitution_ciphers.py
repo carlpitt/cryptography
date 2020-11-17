95e4710a539f103cedf24d5e9d47ad2a3515ff32
carlpitt <66806570+carlpitt@users.noreply.github.com>
2020-11-16 23:58:34 -0500

    Update monoalphabetic_substitution_ciphers.py

--- monoalphabetic_substitution_ciphers.py
+++ monoalphabetic_substitution_ciphers.py
@@ -67,41 +67,110 @@ def mod_mult_inv(a, m):
         x0, x1 = x1, x0 - q * x1
     return x0 % m if b == 1 else -1
 
+
+
 # Caesar cipher
 def caesar_encipher(plaintext, akey):
-    return text_block("".join(chr((ord(i) + akey - 65) % 26 + 65) for i in text_clean(plaintext)))
-   #return text_block("".join(alpha_str[(alpha_str.find(i) + akey) % 26] for i in text_clean(plaintext)))
+    """
+    plaintext: str
+    akey: int
+    returns each letter in plaintext replaced by a letter akey positions down the alphabet
+    """
+    akey %= 26
+    # direct translation using alpha_str
+    #return text_block("".join(alpha_str[(alpha_str.find(i) + akey) % 26] for i in text_clean(plaintext)))
+    
+    # uses chr(65 - 90) and ord("A" - "Z")
+    #return text_block("".join(chr((ord(i) + akey - 65) % 26 + 65) for i in text_clean(plaintext)))
+    
+    # creates alphabet (better for longer messages)
+    caesar_alpha = {alpha_str[i]: chr((i + akey) % 26 + 65) for i in range(26)}
+    return text_block("".join(caesar_alpha[i] for i in text_clean(plaintext)))
 def caesar_decipher(ciphertext, akey):
-    return "".join(chr((ord(i) - akey - 65) % 26 + 65) for i in text_clean(ciphertext)).lower()
-   #return "".join(alpha_str[(alpha_str.find(i) - akey) % 26] for i in text_clean(ciphertext)).lower()
+    """
+    ciphertext: str
+    akey: int
+    returns each letter in ciphertext replaced by a letter akey positions up the alphabet
+    """
+    akey %= 26
+    # direct translation using alpha_str
+    #return "".join(alpha_str[(alpha_str.find(i) - akey) % 26] for i in text_clean(ciphertext)).lower()
+    
+    # uses chr(65 - 90) and ord("A" - "Z")
+    #return "".join(chr((ord(i) - akey - 65) % 26 + 65) for i in text_clean(ciphertext)).lower()
+    
+    # creates alphabet (better for longer messages)
+    caesar_alpha = {alpha_str[i]: chr((i - akey) % 26 + 65) for i in range(26)}
+    return "".join(caesar_alpha[i] for i in text_clean(ciphertext)).lower()
 
 # multiplicative cipher
 def mult_encipher(plaintext, mkey):
-    return text_block("".join(chr((mkey * (ord(i) - 65)) % 26 + 65) for i in text_clean(plaintext)))
-   #return text_block("".join(alpha_str[(mkey * alpha_str.find(i)) % 26] for i in text_clean(plaintext)))
+    """
+    plaintext: str
+    mkey: int
+    returns each letter in plaintext replaced by a letter akey positions down the alphabet
+    """
+    mkey %= 26
+    # direct translation using alpha_str
+    #return text_block("".join(alpha_str[(mkey * alpha_str.find(i)) % 26] for i in text_clean(plaintext)))
+    
+    # uses chr(65 - 90) and ord("A" - "Z")
+    #return text_block("".join(chr((mkey * (ord(i) - 65)) % 26 + 65) for i in text_clean(plaintext)))
+    
+    # creates alphabet (better for longer messages)
+    mult_alpha = {alpha_str[i]: chr((mkey * i) % 26 + 65) for i in range(26)}
+    return text_block("".join(mult_alpha[i] for i in text_clean(plaintext)))
 def mult_decipher(ciphertext, mkey):
-    return "".join(chr(valid_mkeys[mkey] * (ord(i) - 65) % 26 + 65) for i in text_clean(ciphertext)).lower()
-   #return "".join(alpha_str[(valid_mkeys[mkey] * alpha_str.find(i)) % 26] for i in text_clean(ciphertext)).lower()
+    mkey %= 26
+    # direct translation using alpha_str
+    #return "".join(alpha_str[(valid_mkeys[mkey] * alpha_str.find(i)) % 26] for i in text_clean(ciphertext)).lower()
+    
+    # uses chr(65 - 90) and ord("A" - "Z")
+    #return "".join(chr(valid_mkeys[mkey] * (ord(i) - 65) % 26 + 65) for i in text_clean(ciphertext)).lower()
+    
+    # creates alphabet (better for longer messages)
+    mult_alpha = {alpha_str[i]: chr((valid_mkeys[mkey] * i) % 26 + 65) for i in range(26)}
+    return "".join(mult_alpha[i] for i in text_clean(ciphertext)).lower()
 
 # affine cipher
 def affine_encipher(plaintext, mkey, akey):
-    return text_block("".join(chr((mkey * (ord(i) - 65) + akey) % 26 + 65) for i in text_clean(plaintext)))
-   #return text_block("".join(alpha_str[(mkey * alpha_str.find(i) + akey) % 26] for i in text_clean(plaintext)))
+    mkey %= 26
+    akey %= 26
+    # direct translation using alpha_str
+    #return text_block("".join(alpha_str[(mkey * alpha_str.find(i) + akey) % 26] for i in text_clean(plaintext)))
+    
+    # uses chr(65 - 90) and ord("A" - "Z")
+    #return text_block("".join(chr((mkey * (ord(i) - 65) + akey) % 26 + 65) for i in text_clean(plaintext)))
+    
+    # creates alphabet (better for longer messages)
+    affine_alpha = {alpha_str[i]: chr((mkey * i + akey) % 26 + 65) for i in range(26)}
+    return text_block("".join(affine_alpha[i] for i in text_clean(plaintext)))
 def affine_decipher(ciphertext, mkey, akey):
-    return "".join(chr(valid_mkeys[mkey] * (ord(i) - akey - 65) % 26 + 65) for i in text_clean(ciphertext)).lower()
-   #return "".join(alpha_str[(valid_mkeys[mkey] * (alpha_str.find(i) - akey)) % 26] for i in text_clean(ciphertext)).lower()
+    mkey %= 26
+    akey %= 26
+    # direct translation using alpha_str
+    #return "".join(alpha_str[(valid_mkeys[mkey] * (alpha_str.find(i) - akey)) % 26] for i in text_clean(ciphertext)).lower()
+    
+    # uses chr(65 - 90) and ord("A" - "Z")
+    #return "".join(chr(valid_mkeys[mkey] * (ord(i) - akey - 65) % 26 + 65) for i in text_clean(ciphertext)).lower()
+    
+    # creates alphabet (better for longer messages)
+    affine_alpha = {alpha_str[i]: chr((valid_mkeys[mkey] * (i - akey)) % 26 + 65) for i in range(26)}
+    return "".join(affine_alpha[i] for i in text_clean(ciphertext)).lower()
+
+
 
 from seaborn import barplot
 
-alpha_list =          [  "A",    "B",    "C",    "D",    "E",    "F",    "G",
-                         "H",    "I",    "J",    "K",    "L",    "M",    "N",
-                         "O",    "P",    "Q",    "R",    "S",    "T",    "U",
-                         "V",    "W",    "X",    "Y",    "Z"]
+alpha_list =       [  "A",    "B",    "C",    "D",    "E",    "F",    "G",
+                      "H",    "I",    "J",    "K",    "L",    "M",    "N",
+                      "O",    "P",    "Q",    "R",    "S",    "T",    "U",
+                      "V",    "W",    "X",    "Y",    "Z"]
 
-english_letter_freq = (8.167,  1.492,  2.782,  4.253, 12.702,  2.228,  2.015,
-                       6.094,  6.966,  0.153,  0.772,  4.025,  2.406,  6.749,
-                       7.507,  1.929,  0.095,  5.987,  6.327,  9.056,  2.758,
-                       0.978,  2.360,  0.150,  1.974,  0.074)
+letter_freq_list = (8.167,  1.492,  2.782,  4.253, 12.702,  2.228,  2.015,
+                    6.094,  6.966,  0.153,  0.772,  4.025,  2.406,  6.749,
+                    7.507,  1.929,  0.095,  5.987,  6.327,  9.056,  2.758,
+                    0.978,  2.360,  0.150,  1.974,  0.074)
 
 def letter_freq(text):
     """
@@ -122,7 +191,7 @@ def expected_count(text):
     Returns a list of the expected counts of each letter in text
     """
     length = len(text_clean(text))
-    return [length * english_letter_freq[i] / 100 for i in range(26)]
+    return [length * letter_freq_list[i] / 100 for i in range(26)]
 def actual_count(text):
     """
     text: str
